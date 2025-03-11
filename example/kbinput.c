dda03894662118e2fe44fc952290a1fb8b91f33c
binaryfox0 <phamducduy6122011@gmail.com>
2025-03-12 06:15:37 +0700

    Organizing code

--- example/kbinput.c
+++ example/kbinput.c
@@ -1,118 +1,130 @@
-#include <string.h> // strlen
-#include <stdlib.h> // free
+#include <string.h>  // strlen, memcpy
+#include <stdlib.h>  // free
 
 #include "term_display.h"
 #include "term_font.h"
-
 #include "example_utils.h"
 
-char key_pressed[48] = "No key is pressed";
+#define MAX_KEY_STR 48  // Define max key string length
 
-// Table
-static const struct { int mod; const char* text; } mods_lookup[] =
-{
+static char key_pressed[MAX_KEY_STR] = "No key is pressed";
+
+// Modifier key lookup table
+static const struct {
+ int mod;
+ const char* text;
+} mods_lookup[] = {
  { key_ctrl, "Ctrl + " },
  { key_alt,  "Alt + " },
  { key_shift,"Shift + " }
 };
 
-static const char* fkey_name[12] =
-{
- "F1",
- "F2",
- "F3",
- "F4",
- "F5",
- "F6",
- "F7",
- "F8",
- "F9",
- "F10",
- "F11",
- "F12"
+// Function key names
+static const char* fkey_name[] = {
+ "F1", "F2", "F3", "F4", "F5", "F6",
+ "F7", "F8", "F9", "F10", "F11", "F12"
 };
 
-void key_callback(int key, int mods, key_state state)
-{
- memset(key_pressed, 0, 48);
-
+// Key callback function
+void key_callback(int key, int mods, key_state state) {
  int index = 0;
- for(int i = 0; i < sizeof(mods_lookup) / sizeof(mods_lookup[0]); i++)
-  if(mods & mods_lookup[i].mod)
-  {
-   int len = strlen(mods_lookup[i].text);
+
+ // Add modifier keys if present
+ for (size_t i = 0; i < sizeof(mods_lookup) / sizeof(mods_lookup[0]); i++) {
+  if (mods & mods_lookup[i].mod) {
+   size_t len = strlen(mods_lookup[i].text);
    memcpy(&key_pressed[index], mods_lookup[i].text, len);
    index += len;
   }
- if(IN_RANGE(key, term_key_astrophe, term_key_grave_accent))
- {
-  key_pressed[index] = key;
-  index++;
- } else if(IN_RANGE(key, term_key_f1, term_key_f12))
- {
-  const char* key_name = fkey_name[key - term_key_f1];
-  memcpy(&key_pressed[index], key_name, strlen(key_name));
-  index += strlen(key_name);
- } else if(key == term_key_space)
- {
-  memcpy(&key_pressed[index], "Space", 5);
-  index += 5;
- } else if(key == term_key_backspace)
- {
-  memcpy(&key_pressed[index], "Backspace", 9);
-  index += 9;
- } else if(key == term_key_escape)
- {
-  memcpy(&key_pressed[index], "Esc", 3);
-  index += 3;
  }
 
+ // Identify the pressed key
+ if (IN_RANGE(key, term_key_astrophe, term_key_grave_accent)) {
+  key_pressed[index++] = key;
+ } else if (IN_RANGE(key, term_key_f1, term_key_f12)) {
+  const char* key_name = fkey_name[key - term_key_f1];
+  size_t len = strlen(key_name);
+  memcpy(&key_pressed[index], key_name, len);
+  index += len;
+ } else {
+  const char* key_name = NULL;
+
+  switch (key) {
+   case term_key_space:  key_name = "Space"; break;
+   case term_key_backspace: key_name = "Back"; break;
+   case term_key_escape: key_name = "Esc"; break;
+   case term_key_tab: key_name = "Tab"; break;
+   case term_key_enter: key_name = "Enter"; break;
+   case term_key_home: key_name = "Home"; break;
+   case term_key_insert: key_name = "Ins"; break;
+   case term_key_delete: key_name = "Del"; break;
+   case term_key_end: key_name = "End"; break;
+   case term_key_page_up: key_name = "PgUp"; break;
+   case term_key_page_down: key_name = "PgDn"; break;
+   case term_key_up: key_name = "Up"; break;
+   case term_key_left: key_name = "Left"; break;
+   case term_key_down: key_name = "Down"; break;
+   case term_key_right: key_name = "Right"; break;
+  }
+
+  if (key_name) {
+   size_t len = strlen(key_name);
+   memcpy(&key_pressed[index], key_name, len);
+   index += len;
+  }
+ }
+
+ // Append " is pressed."
  memcpy(&key_pressed[index], " is pressed.", 13);
 }
 
-int main()
-{
+// Main function
+int main() {
  u8 enable = 1;
- if(display_init() || start_logging("statics.txt"))
-  return 1;
+ 
+ if (display_init() || start_logging("statics.txt")) return 1;
+
  display_option(auto_resize, 0, &enable);
  display_set_key_callback(key_callback);
 
  u64 frame_count = 0;
- term_texture* texture = 0;
+ term_texture* texture = NULL;
  term_vec2 size = {0};
  const double program_start = get_time();
  double delta_time = 1.0, last_log = get_time();
- while(display_is_running())
- {
+
+ while (display_is_running()) {
   frame_count++;
   double start_frame = get_time();
   double fps = (delta_time > 0) ? (1.0 / delta_time) : 0.0;
- 
-  display_set_color(rgba_init(109, 154, 140, 255)); // Approximtely patina
 
+  display_set_color(rgba_init(109, 154, 140, 255));  // Patina color
   display_poll_events();
 
-  char* string = to_string("%f", fps);
-  texture = display_string_texture(string, strlen(string), &size, rgba_init(255,255,255,255), rgba_init(0,0,0,0));
+  // FPS Display
+  char* fps_str = to_string("%f", fps);
+  texture = display_string_texture(fps_str, strlen(fps_str), &size, 
+           rgba_init(255, 255, 255, 255), rgba_init(0, 0, 0, 0));
   display_copy_texture(texture, pos_init(-1.0f, 1.0f), TEXTURE_MERGE_RESIZE);
   texture_free(texture);
 
-  // The key pressed string
-  texture = display_string_texture(key_pressed, strlen(key_pressed), &size, rgba_init(255,255,255,255), rgba_init(0,0,0,0));
+  // Key Press Display
+  texture = display_string_texture(key_pressed, strlen(key_pressed), &size, 
+           rgba_init(255, 255, 255, 255), rgba_init(0, 0, 0, 0));
   display_copy_texture(texture, pos_init(-1.0f, 0.0f), TEXTURE_MERGE_CROP);
   texture_free(texture);
 
   display_show();
 
   delta_time = get_time() - start_frame;
-  if(start_frame - last_log >= LOG_INTERVAL)
-  {
-   write_log("FPS: %s", string);
+  if (start_frame - last_log >= LOG_INTERVAL) {
+   write_log("FPS: %s", fps_str);
    last_log = get_time();
   }
-  free(string);
+
+  free(fps_str);
  }
+
  display_free();
  stop_logging();
  return 0;
--- src/term_priv_linux.c
+++ src/term_priv_linux.c
@@ -7,221 +7,190 @@
 #include <termios.h>
 #include <poll.h>
 #include <signal.h>
-
 #include <stdio.h>
 
-term_vec2 query_terminal_size()
-{
+static struct termios old, cur;
+static struct pollfd pfd;
+
+term_vec2 query_terminal_size() {
  struct winsize ws;
- if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) != -1)
-  return vec2_init(ws.ws_col, ws.ws_row);
- return vec2_init(0, 0);
+ return (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) != -1) ? vec2_init(ws.ws_col, ws.ws_row) : vec2_init(0, 0);
 }
 
-u8 set_handler(int type, void (*handler)(int))
-{
+u8 set_handler(int type, void (*handler)(int)) {
 #ifdef _POSIX_VERSION
- struct sigaction sa;
- sa.sa_flags = SA_SIGINFO;
+ struct sigaction sa = { .sa_flags = SA_SIGINFO, .sa_handler = handler };
  sigemptyset(&sa.sa_mask);
- sa.sa_handler = handler;
- return (sigaction(type, &sa, 0)!=0);
+ return (sigaction(type, &sa, 0) != 0);
 #else
  return signal(type, handler) == SIG_ERR;
 #endif
 }
 
-struct termios old, cur;
-struct pollfd pfd;
-u8 setup_env(void *stop_handler)
-{
- if(tcgetattr(STDIN_FILENO, &old) == -1)
-  return 1;
+u8 setup_env(void *stop_handler) {
+ if (tcgetattr(STDIN_FILENO, &old) == -1) return 1;
+ 
  cur = old;
  cur.c_lflag &= ~(ICANON | ECHO);
- if(tcsetattr(STDIN_FILENO, TCSANOW, &cur) == -1)
-  return 1;
+ 
+ if (tcsetattr(STDIN_FILENO, TCSANOW, &cur) == -1) return 1;
+
  void (*handler)(int) = (void (*)(int))stop_handler;
- if(
-  set_handler(SIGINT, handler) ||
+ if (set_handler(SIGINT, handler) ||
   set_handler(SIGTERM, handler) ||
-  set_handler(SIGQUIT, handler)
- ) return 1;
+  set_handler(SIGQUIT, handler)) return 1;
+
  pfd.fd = STDIN_FILENO;
  pfd.events = POLLIN;
+ 
  return 0;
 }
 
-u8 restore_env()
-{
- if(tcsetattr(STDIN_FILENO, TCSANOW, &old) == -1)
-  return 1;
- if(
-  set_handler(SIGINT, SIG_DFL) ||
-  set_handler(SIGTERM, SIG_DFL) ||
-  set_handler(SIGQUIT, SIG_DFL)
- ) return 1;
- return 0;
+u8 restore_env() {
+ if (tcsetattr(STDIN_FILENO, TCSANOW, &old) == -1) return 1;
+
+ return (set_handler(SIGINT, SIG_DFL) ||
+   set_handler(SIGTERM, SIG_DFL) ||
+   set_handler(SIGQUIT, SIG_DFL));
 }
 
-static inline u8 handle_single_byte(int *ch, int *mods)
-{
- switch(*ch)
- {
-  case '\0': { *ch = term_key_grave_accent; *mods |= key_ctrl; break; }
-  case 0x1b: { *ch = term_key_escape; break;}
-  case '\"': { *ch = term_key_space; *mods |= key_shift; break; }
-  case '!':
-  case '#':
-  case '$':
-  case '%':
-  {
-   *ch += 16;
-   *mods |= key_shift;
-   break;
-  }
-  case ':': { *ch = term_key_semicolon; *mods |= key_shift; break; }
+#define _getch(ch) if (((ch) = getchar()) == EOF) return
+#define getch_chk(val) if (getchar() != val) return
+
+// Handle single-byte character input
+static inline u8 handle_single_byte(int *ch, int *mods) {
+ switch (*ch) {
+  case '\0':  *ch = term_key_space; *mods |= key_ctrl; break;
+  case 0x08:  *ch = term_key_backspace; *mods |= key_ctrl; break;
+  case 0x09:  *ch = term_key_tab; break;
+  case 0x0A:
+  case 0x0D:  *ch = term_key_enter; break;
+  case 0x1B:  *ch = term_key_escape; break;
+  case '\"':  *ch = term_key_space; *mods |= key_shift; break;
+  case ':':   *ch = term_key_semicolon; *mods |= key_shift; break;
   case '>':
   case '?':
-  case '<': { *ch -= 16; *mods |= key_shift; break; };
-  case '+': { *ch = term_key_equal; *mods |= key_shift; break;}
-  case '@': { *ch = term_key_2; *mods |= key_shift; break; }
-  case '^': { *ch = term_key_6; *mods |= key_shift; break; }
-  case '_': { *ch = term_key_minus; *mods |= key_shift; break; }
-  case '&':
-  case '(': { *ch += 17; *mods |= key_shift; break; }
-  case '*': { *ch = term_key_8; *mods |= key_shift; break; }
-  case ')': { *ch = term_key_0; *mods |= key_shift; break; }
-  case '{':
-  case '|':
-  case '}': { *ch -= 32; *mods |=key_shift; break; }
-  case '~': { *ch = term_key_grave_accent; *mods |= key_shift; break; }
-  case 0x7f: { *ch = term_key_backspace; break; }
+  case '<':   *ch -= 16; *mods |= key_shift; break;
+  case '+':   *ch = term_key_equal; *mods |= key_shift; break;
+  case '@':   *ch = term_key_2; *mods |= key_shift; break;
+  case '^':   *ch = term_key_6; *mods |= key_shift; break;
+  case '_':   *ch = term_key_minus; *mods |= key_shift; break;
+  case 0x7F:  *ch = term_key_backspace; break;
   default:
-  {
-   if(IN_RANGE(*ch, 0x01, 0x1D)) { *ch += 64; *mods |= key_ctrl; break; }
-   if(IN_RANGE(*ch, 'A', 'Z')) { *mods |= key_shift; break; }
-   if(IN_RANGE(*ch, 'a', 'z')) { *ch -= 32; break; }
-   if(IN_RANGE(*ch, ' ', '~')) break; // Remaining characters
-
+   if (IN_RANGE(*ch, 0x01, 0x1D)) { *ch += 64; *mods |= key_ctrl; break; }
+   if (IN_RANGE(*ch, 'A', 'Z')) { *mods |= key_shift; break; }
+   if (IN_RANGE(*ch, 'a', 'z')) { *ch -= 32; break; }
+   if (IN_RANGE(*ch, ' ', '~')) break; // Remaining characters
    return 1;
-  }
  }
  return 0;
 }
 
-// Arrow key + Home/End
-static inline u8 handle_nav_key(int *ch)
-{
- switch(*ch)
- {
-  case 'A': { *ch = term_key_up; break; }
-  case 'B': { *ch = term_key_down; break; }
-  case 'C': { *ch = term_key_right; break; }
-  case 'D': { *ch = term_key_left; break; }
-  case 'H': { *ch = term_key_home; break; }
-  case 'F': { *ch = term_key_end; break; }
+// Handle navigation keys (Arrow keys, Home, End)
+static inline u8 handle_nav_key(int *ch) {
+ switch (*ch) {
+  case 'A': *ch = term_key_up; break;
+  case 'B': *ch = term_key_down; break;
+  case 'C': *ch = term_key_right; break;
+  case 'D': *ch = term_key_left; break;
+  case 'H': *ch = term_key_home; break;
+  case 'F': *ch = term_key_end; break;
   default: return 1;
  }
  return 0;
 }
 
-#define _getch(ch) if(((ch) = getchar()) == EOF) return
-#define getch_chk(val) if(getchar() != val) return
-void kbpoll_events(key_callback_func func)
+static inline u8 handle_f5_below(int* ch)
 {
- if(poll(&pfd, 1, 0) < 1) return;
+ *ch = *ch - 'P' + term_key_f1;
+ if (OUT_RANGE(*ch, term_key_f1, term_key_f4)) return 1;
+ return 0;
+}
 
- int ch = 0;
- int mods = 0, bytes = 0;
- if(ioctl(STDIN_FILENO, FIONREAD, &bytes) == -1 || bytes == 0)
-  return;
+static inline u8 handle_f5_above(int *ch)
+{
+ int first, second;
+ getch_chk('[') 1;
+ _getch(first) 1;
+ _getch(second) 1;
+ getch_chk('~') 1;
+ if (first == '1') {
+  switch (second) {
+   case '5': *ch = term_key_f5; break;
+   case '7': *ch = term_key_f6; break;
+   case '8': *ch = term_key_f7; break;
+   case '9': *ch = term_key_f8; break;
+   default: return 1;
+  }
+ } else if (first == '2') {
+  switch (second) {
+   case '0': *ch = term_key_f9; break;
+   case '1': *ch = term_key_f10; break;
+   case '3': *ch = term_key_f11; break;
+   case '4': *ch = term_key_f12; break;
+   default: return 1;
+  }
+ } else return 1;
+ return 0;
+}
+
+void kbpoll_events(key_callback_func func) {
+ if (poll(&pfd, 1, 0) < 1) return;
+
+ int ch = 0, mods = 0, bytes = 0;
+ if (ioctl(STDIN_FILENO, FIONREAD, &bytes) == -1 || bytes == 0) return;
+
  _getch(ch);
- switch(ch)
- {
-  case 0x0d: { ch = term_key_enter; break; }
-  case 0x1b:
-  {
-   switch(bytes)
-   {
-    case 1: { ch = term_key_escape; break; }
-    case 2: // Alt
-    {
-     mods |= key_alt;
-     _getch(ch);
-     handle_single_byte(&ch, &mods);
-     break;
-    }
-    case 3:
-    {
-     _getch(bytes);
-     _getch(ch);
-     if(bytes == '[') { if(handle_nav_key(&ch)) return; }
-     else if(bytes == 'O')
+
+ if (ch == 0x1B) {  // Escape sequence handling
+  switch (bytes) {
+   case 1: ch = term_key_escape; break;
+   case 2: // Alt modifier
+    mods |= key_alt;
+    _getch(ch);
+    handle_single_byte(&ch, &mods);
+    break;
+   case 3: // Navigation keys & F1-F4
+    _getch(bytes);
+    _getch(ch);
+    if (bytes == '[') {
+     if (handle_nav_key(&ch)) return;
+    } else if (bytes == 'O') {
+     if(handle_f5_below(&ch)) return;
+    } else return;
+    break;
+   case 4: // Page Up / Page Down / Insert / Delete
+    _getch(ch);
+    _getch(bytes);
+   // _getch(ch);
+    if(ch == 'O') {
+     if(bytes == '2')
      {
-      ch = ch - 'P' + term_key_f1;
-      if(ch < term_key_f1 || ch > term_key_f4)
-       return;
+      _getch(ch);
+      mods |= key_shift;
+      if(handle_f5_below(&ch)) return;
      }
-     else return;
-     break;
-    }
-    case 4: // PGUP/PGDN
-    {
-     getch_chk('[');
-     _getch(bytes);
-     getch_chk('~');
-     switch(bytes)
-     {
-      case '2': { ch = term_key_insert; break; }
-      case '3': { ch = term_key_delete; break; }
-      case '5': { ch = term_key_page_up; break; }
-      case '6': { ch = term_key_page_down; break; }
-      default: return;
-     }
-     break;
-    }
-    case 5: // F5 to F12
-    {
-     getch_chk('[');
+    } else if(ch == '[') {
      _getch(ch);
-     _getch(bytes);
-     getch_chk('~');
-     if(ch == '1')
-     {
-      switch(bytes)
-      {
-       case '5': { ch = term_key_f5; break; }
-       case '7': { ch = term_key_f6; break; }
-       case '8': { ch = term_key_f7; break; }
-       case '9': { ch = term_key_f8; break; }
-       default: return;
+     if(ch == '~') {
+       switch (bytes) {
+        case '2': ch = term_key_insert; break;
+        case '3': ch = term_key_delete; break;
+        case '5': ch = term_key_page_up; break;
+        case '6': ch = term_key_page_down; break;
+        default: return;
+       }
       }
-     }
-     else if(ch == '2')
-     {
-      switch(bytes)
-      {
-       case '0': { ch = term_key_f9; break; }
-       case '1': { ch = term_key_f10; break; }
-       case '3': { ch = term_key_f11; break; }
-       case '4': { ch = term_key_f12; break; }
-       default: return;
-      }
-     } else return;
-     break;
-    }
-    default: return;
-   }
-   break;
+    } else return;
+    break;
+   case 5: // Function keys F5 - F12
+    if(handle_f5_above(&ch)) return;
+    break;
+   case 7:
+    break;
+   default: return;
   }
-  case 0x7f: { ch = term_key_backspace; break; }
-  default:
-  {
-   bytes = ch;
-   if(handle_single_byte(&ch, &mods)) return;
-   break;
-  }
- }
+ } else // Single-byte characters
+  if (handle_single_byte(&ch, &mods)) return;
  func(ch, mods, key_press);
 }
