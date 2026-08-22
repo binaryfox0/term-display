048c9b023e21e96c8f209a84b952fda6d6b4f27b
binaryfox0 <phamducduy6122011@gmail.com>
2026-08-22 08:29:40 +0700

    add fill full 64x64 block

--- softrast/src/swrz_pool.c
+++ softrast/src/swrz_pool.c
@@ -107,7 +107,14 @@ static void swrz__pool_fill_impl(
     }
 }
 
-
+typedef struct
+{ 
+    int64_t x0;
+    int64_t y0;
+    int64_t x1;
+    int64_t y1;
+} swrz__rectf_t;
+    
 typedef struct
 {
     uint32_t x0;
@@ -116,9 +123,15 @@ typedef struct
     uint32_t y1;
 } swrz__rect_t;
 
-SWRZ__INLINE uint16_t swrz__sign_bit(
-        const int64_t x) {
-    return (uint16_t)(x >> (sizeof(x) * 8 - 1));
+#define SWRZ__SIGN_BIT(x) ((x) >> (sizeof((x)) * 8 - 1))
+
+SWRZ__INLINE swrz__rect_t swrz__rectf_to_rect(
+        const swrz__rectf_t *rect) {
+    return (swrz__rect_t){
+            .x0 = (uint32_t)(rect->x0 >> SWRZ__FIXED_FRAC_BITS),
+            .y0 = (uint32_t)(rect->y0 >> SWRZ__FIXED_FRAC_BITS),
+            .x1 = (uint32_t)(rect->x1 >> SWRZ__FIXED_FRAC_BITS),
+            .y1 = (uint32_t)(rect->y1 >> SWRZ__FIXED_FRAC_BITS)};
 }
 
 static uint16_t swrz__build_masks_scalar(
@@ -137,22 +150,22 @@ static uint16_t swrz__build_masks_scalar(
     c2 = c + 2 * b;
     c3 = c + 3 * b;
 
-    mask |= swrz__sign_bit(c0 + 0 * a) & (1 << 0);
-    mask |= swrz__sign_bit(c0 + 1 * a) & (1 << 1);
-    mask |= swrz__sign_bit(c0 + 2 * a) & (1 << 2);
-    mask |= swrz__sign_bit(c0 + 3 * a) & (1 << 3);
-    mask |= swrz__sign_bit(c1 + 0 * a) & (1 << 4);
-    mask |= swrz__sign_bit(c1 + 1 * a) & (1 << 5);
-    mask |= swrz__sign_bit(c1 + 2 * a) & (1 << 6);
-    mask |= swrz__sign_bit(c1 + 3 * a) & (1 << 7);
-    mask |= swrz__sign_bit(c2 + 0 * a) & (1 << 8);
-    mask |= swrz__sign_bit(c2 + 1 * a) & (1 << 9);
-    mask |= swrz__sign_bit(c2 + 2 * a) & (1 << 10);
-    mask |= swrz__sign_bit(c2 + 3 * a) & (1 << 11);
-    mask |= swrz__sign_bit(c3 + 0 * a) & (1 << 12);
-    mask |= swrz__sign_bit(c3 + 1 * a) & (1 << 13);
-    mask |= swrz__sign_bit(c3 + 2 * a) & (1 << 14);
-    mask |= swrz__sign_bit(c3 + 3 * a) & (1 << 15);
+    mask |= SWRZ__SIGN_BIT(c0 + 0 * a) & (1 << 0);
+    mask |= SWRZ__SIGN_BIT(c0 + 1 * a) & (1 << 1);
+    mask |= SWRZ__SIGN_BIT(c0 + 2 * a) & (1 << 2);
+    mask |= SWRZ__SIGN_BIT(c0 + 3 * a) & (1 << 3);
+    mask |= SWRZ__SIGN_BIT(c1 + 0 * a) & (1 << 4);
+    mask |= SWRZ__SIGN_BIT(c1 + 1 * a) & (1 << 5);
+    mask |= SWRZ__SIGN_BIT(c1 + 2 * a) & (1 << 6);
+    mask |= SWRZ__SIGN_BIT(c1 + 3 * a) & (1 << 7);
+    mask |= SWRZ__SIGN_BIT(c2 + 0 * a) & (1 << 8);
+    mask |= SWRZ__SIGN_BIT(c2 + 1 * a) & (1 << 9);
+    mask |= SWRZ__SIGN_BIT(c2 + 2 * a) & (1 << 10);
+    mask |= SWRZ__SIGN_BIT(c2 + 3 * a) & (1 << 11);
+    mask |= SWRZ__SIGN_BIT(c3 + 0 * a) & (1 << 12);
+    mask |= SWRZ__SIGN_BIT(c3 + 1 * a) & (1 << 13);
+    mask |= SWRZ__SIGN_BIT(c3 + 2 * a) & (1 << 14);
+    mask |= SWRZ__SIGN_BIT(c3 + 3 * a) & (1 << 15);
 
     /* inverted */
     return (uint16_t)~mask;
@@ -169,7 +182,7 @@ static void swrz__pool_partial(
 {
 }
 */
-
+#define SWRZ__MAX_PLANES 3
 
 swrz_error_t swrz__pool_rasterize_triangle(
         swrz__pool_t *pool,
@@ -178,6 +191,149 @@ swrz_error_t swrz__pool_rasterize_triangle(
         const swrz_vertex_output_t *v1,
         const swrz_vertex_output_t *v2)
 {
+    int64_t v0_x = 0, v1_x = 0, v2_x = 0;
+    int64_t v0_y = 0, v1_y = 0, v2_y = 0;
+    int64_t area = 0;
+
+    swrz__rectf_t bboxf = {0};
+    swrz__rect_t bbox = {0};
+    swrz__rect_t bbox_tile = {0};
+
+    int64_t tri_a[SWRZ__MAX_PLANES] = {0}, 
+            tri_b[SWRZ__MAX_PLANES] = {0},
+            tri_c[SWRZ__MAX_PLANES] = {0},
+            tri_eo[SWRZ__MAX_PLANES] = {0},
+            tri_ei[SWRZ__MAX_PLANES] = {0};
+
+    int64_t a[SWRZ__MAX_PLANES] = {0},
+            b[SWRZ__MAX_PLANES] = {0},
+            c[SWRZ__MAX_PLANES] = {0},
+            eo[SWRZ__MAX_PLANES] = {0},
+            ei[SWRZ__MAX_PLANES] = {0};
+
+    /* we inverted y position because we are mimick OpenGL */
+    v0_x = (int32_t)((v0->position[0] / v0->position[3] + 1.0f)
+        * 0.5f * (float)fb->width * SWRZ__FIXED_ONE);
+    v0_y = (int32_t)((1.0f - v0->position[1] / v0->position[3])
+            * 0.5f * (float)fb->height * SWRZ__FIXED_ONE);
+    v1_x = (int32_t)((v1->position[0] / v1->position[3] + 1.0f)
+            * 0.5f * (float)fb->width * SWRZ__FIXED_ONE);
+    v1_y = (int32_t)((1.0f - v1->position[1] / v1->position[3])
+            * 0.5f * (float)fb->height * SWRZ__FIXED_ONE);
+    v2_x = (int32_t)((v2->position[0] / v2->position[3] + 1.0f)
+            * 0.5f * (float)fb->width * SWRZ__FIXED_ONE);
+    v2_y = (int32_t)((1.0f - v2->position[1] / v2->position[3])
+            * 0.5f * (float)fb->height * SWRZ__FIXED_ONE);
+
+    /* inverted */
+    area = (int64_t)(v1_x - v0_x) * (v0_y - v2_y) -
+        (int64_t)(v0_y - v1_y) * (v2_x - v0_x);
+    if(area < 0)
+        return SWRZ_ERR_OK;
+    
+    bboxf.x0 = SWRZ__MIN3(v0_x, v1_x, v2_x);
+    bboxf.y0 = SWRZ__MIN3(v0_y, v1_y, v2_y);
+    bboxf.x1 = SWRZ__MAX3(v0_x, v1_x, v2_x);
+    bboxf.y1 = SWRZ__MAX3(v0_y, v1_y, v2_y);
+
+    bboxf.x0 = SWRZ__MAX(bboxf.x0, 0);
+    bboxf.y0 = SWRZ__MAX(bboxf.y0, 0);
+    bboxf.x1 = SWRZ__MIN(bboxf.x1, (int64_t)fb->width << SWRZ__FIXED_FRAC_BITS);
+    bboxf.y1 = SWRZ__MIN(bboxf.y1, (int64_t)fb->height << SWRZ__FIXED_FRAC_BITS);
+
+    bbox = swrz__rectf_to_rect(&bboxf);
+    bbox_tile.x0 = bbox.x0 >> SWRZ__TILE_ORDER;
+    bbox_tile.y0 = bbox.y0 >> SWRZ__TILE_ORDER;
+    bbox_tile.x1 = bbox.x1 >> SWRZ__TILE_ORDER;
+    bbox_tile.y1 = bbox.y1 >> SWRZ__TILE_ORDER;
+
+    // coefficients was invertes due to inverted y coords
+tri_a[0] = v1_y - v0_y;
+tri_a[1] = v2_y - v1_y;
+tri_a[2] = v0_y - v2_y;
+
+tri_b[0] = v0_x - v1_x;
+tri_b[1] = v1_x - v2_x;
+tri_b[2] = v2_x - v0_x;
+
+tri_c[0] = v1_x * v0_y - v0_x * v1_y;
+tri_c[1] = v2_x * v1_y - v1_x * v2_y;
+tri_c[2] = v0_x * v2_y - v2_x * v0_y;
+
+    for(int i = 0; i < SWRZ__MAX_PLANES; i++)
+    {
+        if(tri_a[i] > 0)
+            tri_eo[i] += tri_a[i];
+        else
+            tri_ei[i] -= tri_a[i];
+
+        if(tri_b[i] > 0)
+            tri_eo[i] += tri_b[i];
+        else
+            tri_ei[i] -= tri_b[i];
+    }
+
+    for(int i = 0; i < SWRZ__MAX_PLANES; i++)
+    {
+        // mul by TILE_SIZE, not lshift TILE_ORDER to prevent UB
+        a[i] = tri_a[i] * SWRZ__TILE_SIZE;
+        b[i] = tri_b[i] * SWRZ__TILE_SIZE;
+        c[i] = tri_c[i] + tri_a[i] * bboxf.x0 + tri_b[i] * bboxf.y0;
+        eo[i] = tri_eo[i] * SWRZ__TILE_SIZE;
+        ei[i] = tri_ei[i] * SWRZ__TILE_SIZE;
+    }
+
+    for(uint32_t tile_y = bbox_tile.y0; tile_y < bbox_tile.y1; tile_y++)
+    {
+        bool in = false; // inside triangle for this row
+        int64_t cx[SWRZ__MAX_PLANES] = {0};
+        for(int i = 0; i < SWRZ__MAX_PLANES; i++)
+            cx[i] = c[i];
+
+        for(uint32_t tile_x = bbox_tile.x0; tile_x < bbox_tile.x1; tile_x++)
+        {
+            int out = 0, partial = 0;
+            for(int i = 0; i < SWRZ__MAX_PLANES; i++)
+            {
+                int64_t plane_out = 0, plane_partial = 0;
+                plane_out = cx[i] + eo[i];
+                plane_partial = cx[i] + ei[i] - 1;
+                out |= SWRZ__SIGN_BIT(plane_out);
+                partial |= SWRZ__SIGN_BIT(plane_partial) & (1 << i);
+            }
+
+            if(out)
+            {
+                printf("out\n");
+                if(in)
+                    break; // nothing left, exit...
+            } else if(partial) {
+                printf("partial\n");
+                in = true;
+            } else {
+                printf("full\n");
+                in = true;
+                swrz__pool_fill_impl(fb, 
+                        tile_x * SWRZ__TILE_SIZE,
+                        tile_y * SWRZ__TILE_SIZE,
+                        SWRZ__TILE_SIZE, SWRZ__TILE_SIZE,
+                        0xff0000ff);
+            }
+
+            for(int i = 0; i < SWRZ__MAX_PLANES; i++)
+                cx[i] += a[i];
+        }
+
+        for(int i = 0; i < SWRZ__MAX_PLANES; i++)
+            c[i] += b[i];
+    }
+
+    (void)bbox_tile;
+    (void)pool;
+    (void)swrz__pool_fill_impl;
+    (void)swrz__build_masks_scalar;
+    
+    return SWRZ_ERR_OK;
 }
 
 void swrz__pool_destroy(
